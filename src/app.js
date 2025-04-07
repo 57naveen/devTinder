81ac23da94a22ef95a63fc5cb9501fb43687745f
57naveen <naveenkishore65@gmail.com>
2025-04-07 19:17:44 +0530

    Schema  validation done

--- src/app.js
+++ src/app.js
@@ -4,12 +4,11 @@ const User = require("./models/user");
 
 const app = express();
 
-//This the middleware for convert the json data 
+//This the middleware for convert the json data
 app.use(express.json());
 
 // API for User sign up
 app.post("/signup", async (req, res) => {
-
   // Creating a new instance of the User model
   const user = new User(req.body);
 
@@ -18,6 +17,77 @@ app.post("/signup", async (req, res) => {
     res.send("User Added Successfully!");
   } catch (error) {
     res.status(400).send("Error saving the user:" + error.message);
+  }
+});
+
+//Get user by emailId
+app.get("/user", async (req, res) => {
+  const email = req.body.emailId;
+
+  try {
+    const user = await User.find({ emailId: email });
+
+    if (user.length > 0) {
+      res.send(user);
+    } else {
+      res.status(400).send("User not Found");
+    }
+  } catch (error) {
+    res.status(400).send("Something went wrong !");
+  }
+});
+
+//Feed API - GET/feed - get all the user from the database
+app.get("/feed", async (req, res) => {
+  try {
+    //This empty '{}' object will get the all the user from the database
+    const users = await User.find({});
+
+    res.send(users);
+  } catch (error) {
+    res.status(400).send("Something Went wrong !");
+  }
+});
+
+//Delete the user by id
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+
+  try {
+    const user = await User.findByIdAndDelete(userId);
+
+    res.send("User deleted successfully...");
+  } catch (error) {
+    res.status(400).send("Something went wrong!!!");
+  }
+});
+
+//Update user API
+app.patch("/user/:userId", async (req, res) => {
+  const userId = req.params?.userId;
+  const data = req.body;
+
+  try {
+    const ALLOWED_UPDATES = ["PhotoUrl", "about", "gender", "age", "skills"];
+    const isUpdateAllowed = Object.keys(data).every((k) => {
+      ALLOWED_UPDATES.includes(k);
+    });
+
+    if (!isUpdateAllowed) {
+      throw new Error("Update not allowed");
+    }
+    
+    //validation for skills field 
+    if(data?.skills.length > 10){
+      throw new Error("Skills cannot be more than 10 ")
+    }
+
+    await User.findByIdAndUpdate({ _id: userId }, data, {
+      runValidators: true,
+    });
+    res.send("User Updated Successfully...");
+  } catch (error) {
+    res.status(400).send("Update Failed : " + error.message);
   }
 });
 
--- src/models/user.js
+++ src/models/user.js
@@ -1,24 +1,74 @@
 const mongoose = require("mongoose");
+const validator = require("validator")
 
 const userSchema = new mongoose.Schema({
   firstName: {
     type: String,
+    required:true,
+    minlength:4,
+    maxlength:50,
   },
   lastName: {
     type: String,
   },
   emailId: {
     type: String,
+    lowercase:true,
+    required:true,
+    unique:true,
+    trim:true,
+    validate(value)
+    {
+       if(!validator.isEmail(value))
+       {
+        throw new Error("Invalid email address "+ value);
+       }
+    }
   },
   password: {
     type: String,
+    required:true,
+    validate(value)
+    {
+       if(!validator.isStrongPassword(value))
+       {
+        throw new Error("Enter a Strong Password "+ value);
+       }
+    }
   },
   age: {
     type: Number,
+    min:18
   },
   gender: {
     type: String,
+    validate(value){
+      if(!["male","female","others"].includes(value))
+      {
+        throw new Error("Gender data is not Valid")
+      }
+    }
   },
+  photoUrl : {
+    type:String,
+    default:"https://www.cgg.gov.in/wp-content/uploads/2017/10/dummy-profile-pic-male1.jpg",
+    validate(value)
+    {
+       if(!validator.isURL(value))
+       {
+        throw new Error("Invalid image urlA address "+ value);
+       }
+    }
+  },
+  about:{
+    type:String,
+    default:"This is the default value of the user"
+  },
+  skills:{
+    type:[String],
+  }
+},{
+  timestamps:true,
 });
 
 module.exports = mongoose.model("User", userSchema);
